b2b8322ba9c3abbbf5cb69249042f8b95bb264f3
CX <cx@CXs-iMac.local>
2026-08-30 11:55:36 +0800

    Align three-number forecast arrow

--- app/DynamicWuxingPoster.tsx
+++ app/DynamicWuxingPoster.tsx
@@ -282,7 +282,9 @@ export default function DynamicWuxingPoster({
                       className="prediction-arrow"
                       key={`prediction-${branchIndex}-${position}`}
                       d={genericMulti
-                        ? `M ${columnX(position)} ${rowY(orderedDraws[0].period) - 18} C ${columnX(position)} 225, 345 205, 300 190`
+                        ? item.branches.length === 3
+                          ? `M ${columnX(position)} ${rowY(orderedDraws[0].period) - 18} C ${columnX(position)} 150, 345 112, 300 102`
+                          : `M ${columnX(position)} ${rowY(orderedDraws[0].period) - 18} C ${columnX(position)} 225, 345 205, 300 190`
                         : `M ${columnX(position)} ${rowY(orderedDraws[0].period) - 18} C ${columnX(position)} 165, 430 138, 455 ${102 + branchIndex * 22}`}
                       markerEnd="url(#prediction-arrow-head)"
                     />
